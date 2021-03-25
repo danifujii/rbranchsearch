b028afd772c5d72cae46b9e5ab4dbd0bdcc10aa7
danifujii <danifujii@gmail.com>
2021-03-25 12:34:56 -0300

    Input move branches (#2)
    * Print available branches
    
    * Refactor to use queue and execute macros
    
    * Add main loop
    
    * Format
    
    * Move to modules
    
    * Extract another function
    
    * Extract into even more functions

---
+++ src/gui.rs
@@ -0,0 +1,31 @@
+use crossterm::{cursor, execute, queue, style, terminal, Result};
+use std::io::{Stdout, Write};
+
+pub fn display_closing_error(mut stdout: &Stdout, err: String) -> Result<()> {
+    terminal::disable_raw_mode()?;
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0),
+        style::Print(err)
+    )?;
+    Ok(())
+}
+
+pub fn write_line(mut stdout: &Stdout, line: &String, idx: u16) -> Result<()> {
+    execute!(
+        stdout,
+        cursor::MoveTo(0, idx),
+        terminal::Clear(terminal::ClearType::CurrentLine),
+        style::Print(line)
+    )?;
+    Ok(())
+}
+
+pub fn write_lines(mut stdout: &Stdout, lines: &Vec<String>) -> Result<()> {
+    for i in 0..lines.len() {
+        queue!(stdout, style::Print(&lines[i]), cursor::MoveToNextLine(1))?;
+    }
+    stdout.flush()?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,95 @@
-use crossterm::{cursor, execute, queue, style, terminal, Result};
-use std::io::{stdout, Write};
-use std::process::Command;
-use std::str;
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, terminal, Result,
+};
+use std::io::{stdout, Stdout};
 
-fn get_branches() -> Vec<String> {
-    let cmd = Command::new("git")
-        .arg("branch")
-        .output()
-        .expect("Failed to obtain branches: could not execute command");
+mod git;
+mod gui;
 
-    if cmd.status.success() {
-        let s = match str::from_utf8(&cmd.stdout) {
-            Ok(v) => v.to_owned(),
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-        return s
-            .replace("*", " ")
-            .split("\n")
-            .map(|s| s.to_string())
-            .collect();
-    }
-    panic!("Failed to obtain branches: cmd unsucessful");
+const SELECTED_INDICATOR: char = '*';
+
+fn draw_selected_branch(stdout: &Stdout, branches: &Vec<String>, selected: usize) -> Result<()> {
+    let branch: String = (&branches[selected]).chars().skip(1).collect();
+    let selected_branch: String = format!("{}{}", SELECTED_INDICATOR, branch);
+    gui::write_line(&stdout, &selected_branch, selected as u16)?;
+    Ok(())
 }
 
-fn main() -> Result<()> {
-    let mut stdout = stdout();
+fn update_selected_branch(
+    stdout: &Stdout,
+    branches: &Vec<String>,
+    selected: usize,
+    up: bool,
+) -> Result<()> {
+    gui::write_line(&stdout, &branches[selected], selected as u16)?; // Reset previous selected
+    let new_selected = if up { selected - 1 } else { selected + 1 };
+    draw_selected_branch(&stdout, &branches, new_selected)?;
+    Ok(())
+}
+
+fn setup(mut stdout: &Stdout, branches: &Vec<String>) -> Result<()> {
     queue!(
         stdout,
         terminal::Clear(terminal::ClearType::All),
         cursor::Hide,
         cursor::MoveTo(0, 0)
     )?;
+    gui::write_lines(&stdout, &branches)?;
+    draw_selected_branch(&stdout, &branches, 0)?;
+    Ok(())
+}
 
-    let branches = get_branches();
-    for i in 0..branches.len() {
-        queue!(
-            stdout,
-            style::Print(&branches[i]),
-            cursor::MoveToNextLine(1)
-        )?;
+fn main_loop(stdout: &Stdout, branches: &Vec<String>) -> Result<()> {
+    let mut selected_branch: usize = 0;
+    loop {
+        if let Ok(Event::Key(KeyEvent { code: kc, .. })) = event::read() {
+            match kc {
+                KeyCode::Up => {
+                    if selected_branch > 0 {
+                        update_selected_branch(&stdout, &branches, selected_branch, true)?;
+                        selected_branch -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if selected_branch < branches.len() - 1 {
+                        update_selected_branch(&stdout, &branches, selected_branch, false)?;
+                        selected_branch += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    if let Err(s) = git::change_branch(branches[selected_branch].trim().to_string())
+                    {
+                        gui::display_closing_error(&stdout, s)?;
+                    }
+                    break;
+                }
+                KeyCode::Char(c) => {
+                    if c == 'q' {
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
     }
+    Ok(())
+}
 
-    stdout.flush()?;
+fn cleanup(mut stdout: &Stdout) -> Result<()> {
+    terminal::disable_raw_mode()?;
+    execute!(stdout, cursor::Show)?;
+    Ok(())
+}
 
-    execute!(
-        stdout,
-        cursor::Show
-    )?;
+fn main() -> Result<()> {
+    terminal::enable_raw_mode()?;
 
+    let branches = git::get_branches();
+    let stdout = stdout();
+    setup(&stdout, &branches)?;
+    main_loop(&stdout, &branches)?;
+    cleanup(&stdout)?;
     Ok(())
 }
